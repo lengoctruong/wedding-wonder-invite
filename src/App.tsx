6f3f7b2aaf1a5e3400450ba9e0719fb125289c05
Truong Le <lengoctruong1994@gmail.com>
2025-09-14 09:55:51 +0700

    fix(router): set basename for GitHub Pages & replace internal anchors

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <ImageProtection />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
